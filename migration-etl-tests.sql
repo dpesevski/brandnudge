9ba68442ab74fec3be482799c4ad572b1e368f66
Dejan Peshevski <dpesevski@gmail.com>
2024-05-29 15:30:40 +0200

    etl test changes

--- migration-etl-tests.sql
+++ migration-etl-tests.sql
@@ -2,40 +2,30 @@
 TRUNCATE staging.retailer_daily_data;
 TRUNCATE staging.debug_errors;
 TRUNCATE staging.debug_test_run;
- */
-DROP TABLE IF EXISTS staging_bck.tests_daily_data_pp;
-CREATE TABLE staging_bck.tests_daily_data_pp AS
-SELECT debug_test_run_id,
-       product.date,
-       retailer,
-       product."countryCode",
-       product."currency",
-       product."sourceId",
-       product.ean,
-       product."brand",
-       product."title",
-       fn_to_float(product."shelfPrice")  AS "shelfPrice",
-       fn_to_float(product."wasPrice")    AS "wasPrice",
-       fn_to_float(product."cardPrice")   AS "cardPrice",
-       fn_to_boolean(product."inStock")   AS "inStock",
-       fn_to_boolean(product."onPromo")   AS "onPromo",
-       product."promoData",
-       product."skuURL",
-       product."imageURL",
-       fn_to_boolean(product."bundled")   AS "bundled",
-       fn_to_boolean(product."masterSku") AS "masterSku"
-FROM staging.retailer_daily_data
-         CROSS JOIN LATERAL JSON_POPULATE_RECORDSET(NULL::staging.retailer_data_pp,
-                                                    fetched_data #> '{products}') AS product
-WHERE flag = 'create-products-pp';
-
-SELECT COUNT(*)
-FROM staging_bck.tests_daily_data_pp;
 
 
 SELECT staging.load_retailer_data(fetched_data, flag)
 --SELECT *
 FROM staging.debug_errors;
+
+ */
+
+WITH prod_cnt AS (SELECT test_run_id AS debug_test_run_id, COUNT(*) AS product_count
+                  FROM staging.debug_products
+                  GROUP BY test_run_id)
+SELECT debug_test_run_id,
+       --  fetched_data,
+       product_count,
+       flag,
+       created_at
+FROM staging.retailer_daily_data
+         INNER JOIN prod_cnt USING (debug_test_run_id)
+ORDER BY debug_test_run_id DESC;
+
+SELECT COUNT(*) AS product_count
+FROM staging.debug_products;
+
+
 
 SELECT test_run_id, COUNT(*)
 FROM staging.debug_products
@@ -68,13 +58,6 @@ FROM debug_errors
          LEFT OUTER JOIN debug_test_run
                          USING (debug_test_run_id)
 ORDER BY error_id;
-
-SELECT debug_test_run_id,
-       --  fetched_data,
-       flag,
-       created_at
-FROM staging.retailer_daily_data
-ORDER BY debug_test_run_id DESC;
 
 
 
