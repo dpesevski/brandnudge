87ab2e3405f412d7ede10fd0619d647a499d825f
Dejan Peshevski <dpesevski@gmail.com>
2024-06-14 11:23:17 +0200

    prod_fdw

--- fdw.sql
+++ fdw.sql
@@ -1,10 +1,7 @@
 CREATE EXTENSION IF NOT EXISTS postgres_fdw;
-CREATE SERVER devdb_fdw FOREIGN DATA WRAPPER postgres_fdw OPTIONS (host 'brandnudge-db-cluster-test-v2.cluster-cgtow2b7iejj.eu-north-1.rds.amazonaws.com', port '5432', dbname 'brandnudge-dev');
-CREATE USER MAPPING FOR postgres SERVER devdb_fdw OPTIONS (user 'postgres', password 'fPQWtdGp2zMe4NNr');
+CREATE SERVER proddb_fdw FOREIGN DATA WRAPPER postgres_fdw OPTIONS (host 'brandnudge-db-cluster-prod.cluster-cgtow2b7iejj.eu-north-1.rds.amazonaws.com', port '5432', dbname 'brandnudge');
+CREATE USER MAPPING FOR postgres SERVER proddb_fdw OPTIONS (user 'dejan_user', password 'nCIqhxXgwItIGtK');
 
-IMPORT FOREIGN SCHEMA staging FROM SERVER devdb_fdw INTO dev_staging;
+CREATE SCHEMA prod_fdw;
+IMPORT FOREIGN SCHEMA public FROM SERVER proddb_fdw INTO prod_fdw;
 
-
-CREATE TABLE staging.dev_retailer_daily_data AS
-SELECT *
-FROM dev_staging.retailer_daily_data
