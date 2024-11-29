3e9b41257722e54c89cf1367c9596cb987c05d33
Dejan Peshevski <dpesevski@gmail.com>
2024-11-29 22:27:40 +0100

    migrate_retailer

--- ETL/LOADS ARE EXECUTED CONCURENTLY BY NODEJS ETL-LOCKS-PARTITION BY RETAILER.sql
+++ ETL/LOADS ARE EXECUTED CONCURENTLY BY NODEJS ETL-LOCKS-PARTITION BY RETAILER.sql
@@ -1,4 +1,11 @@
 SELECT "retailerId", COUNT(*), MIN("createdAt"), MAX("createdAt")
 FROM products
 WHERE "dateId" = 29980
-GROUP BY "retailerId"
+GROUP BY "retailerId";
+
+
+/*  other notes
+    1. productStatuss FK to products is expensive. Both to check if productId exist there
+
+
+*/
